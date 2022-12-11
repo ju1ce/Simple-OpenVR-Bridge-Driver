56b85927c1001d345cb01d1bc761e2470a1a3b54
bobsayshilol <bobsayshilol@live.co.uk>
2022-12-11 20:48:36 +0000

    Remove unnecessary re-declaration of base class methods
    We're not overriding them here so we don't need to expose them.

--- driver_files/src/Driver/IVRDevice.hpp
+++ driver_files/src/Driver/IVRDevice.hpp
@@ -50,14 +50,6 @@ namespace ExampleDriver {
             return out_pose;
         }
 
-        // Inherited via ITrackedDeviceServerDriver
-        virtual vr::EVRInitError Activate(uint32_t unObjectId) = 0;
-        virtual void Deactivate() = 0;
-        virtual void EnterStandby() = 0;
-        virtual void* GetComponent(const char* pchComponentNameAndVersion) = 0;
-        virtual void DebugRequest(const char* pchRequest, char* pchResponseBuffer, uint32_t unResponseBufferSize) = 0;
-        virtual vr::DriverPose_t GetPose() = 0;
-
         ~IVRDevice() = default;
     };
 };
